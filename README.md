116a284ecaf1ba8de28b7bac1455958c54189638
Wyatt Allen <wyatt.allen0@gmail.com>
2013-10-29 10:55:18 -0700

    Added script

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-contributers
+contributors
 ============
 
-Embed a list of GitHub contributors in your project page
+Embed a list of GitHub contributors in your project page as links to their 
+GutHub profiles.
+
+# Example
+
+    $("#target-element").contributors("whatgoodisaroad", "contributers");
+
---
+++ contributors.js
@@ -0,0 +1,21 @@
+$.fn.contributors = function(username, reponame) {
+  var 
+    elem = this,
+    url = [
+      "https://api.github.com/repos/", 
+      username, "/", reponame, 
+      "/contributors?callback=?" 
+    ].join("");
+
+  $.getJSON(url, function(result) {
+    for (var idx = 0; idx < result.data.length; ++idx) {
+      var contributor = result.data[idx];
+      if (contributor.login !== username) {
+        $("<a/>&nbsp;")
+          .attr("href", contributor.html_url)
+          .text(contributor.login)
+          .appendTo(elem);
+      }
+    }
+  });
+}
